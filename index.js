f3d76d3ab32fa2fdf6f1b357e3e499ae23c1ca29
Stackgeeks Labs <stackgeekslabs@Stackgeekss-MacBook-Air.local>
2024-02-29 12:00:03 +0530

    initial

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
+import messaging from '@react-native-firebase/messaging';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+messaging().getInitialNotification(async remoteMessage => {
+  console.log('Message handled in the kill mode!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
